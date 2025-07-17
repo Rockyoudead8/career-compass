bb34fcae7b5eb41558d7d437e847dbc6d8e5570d
Rockyoudead8 <arpitgoyal1941977@gmail.com>
2025-07-17 12:57:41 +0530

    Disable ESLint for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['randomuser.me', 'images.unsplash.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
